aefce98c801eccc9d93e2f2b141c983842fc6ac1
whoany <120975118+whoany@users.noreply.github.com>
2026-05-23 18:29:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,23 @@
 #include <iostream>
 #include <cstdlib>
 #include "train.h"
- 
+
 int main() {
   std::cout << "n,all_off,all_on,random_avg" << std::endl;
   std::srand(1);
  
   for (int n = 10; n <= 500; n += 10) {
     int count;
-
     Train tOff;
     count = n;
     while (count--)
       tOff.addCar(false);
     tOff.getLength();
- 
     Train tOn;
     count = n;
     while (count--)
       tOn.addCar(true);
     tOn.getLength();
-    
     int sum = 0;
     for (int k = 0; k < 30; k++) {
       Train tRnd;
@@ -32,7 +29,7 @@ int main() {
       sum += tRnd.getOpCount();
     }
     double avg = sum / 30.0;
- 
+   
     std::cout << n << "," << tOff.getOpCount() << ","
               << tOn.getOpCount() << "," << avg << std::endl;
   }
